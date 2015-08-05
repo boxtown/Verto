934e52ffcfe7ee180f49e32f7d88ecc2b7f7d445
Michael Ma <michael60612@gmail.com>
2015-08-04 21:36:56 -0400

    Context get set now uses Request.Form and includes POST PUT form params

--- context.go
+++ context.go
@@ -22,8 +22,6 @@ type Context struct {
 	// The original *http.Request
 	Request *http.Request
 
-	params url.Values
-
 	// This field is populated by Verto based on user
 	// set injections.
 	Injections *Injections
@@ -31,39 +29,48 @@ type Context struct {
 	// If Verto has a registered Logger, it can be
 	// accessed here.
 	Logger Logger
+
+	params url.Values
 }
 
 // Get retrieves the request parameter associated with
-// key or an error if Context was not properly initialized.
+// key or an error if Context was not properly initialized or if there
+// was an error retrieving parameters from the Request
 func (c *Context) Get(key string) (string, error) {
 	if c.Request == nil {
 		return "", ErrContextNotInitialized
 	}
 
 	if c.params == nil {
-		c.params = c.Request.URL.Query()
+		if err := c.Request.ParseForm(); err != nil {
+			return err
+		}
+		c.params = c.Request.Form
 	}
-
 	return c.params.Get(key), nil
 }
 
 // GetMulti returns the a slice containing all relevant parameters
-// tied to key or an error if Context was not properly initialized.
+// tied to key or an error if Context was not properly initialized or if there
+// was an error retrieving parameters from the Request
 func (c *Context) GetMulti(key string) ([]string, error) {
 	if c.Request == nil {
 		return nil, ErrContextNotInitialized
 	}
 
 	if c.params == nil {
-		c.params = c.Request.URL.Query()
+		if err := c.Request.ParseForm(); err != nil {
+			return err
+		}
+		c.params = c.Request.Form
 	}
-
 	return c.params[key], nil
 }
 
 // GetBool retrieves the value associated with key as a bool
-// or returns an error if the conversion failed or Context
-// was not properly initialized.
+// or returns an error if the conversion failed, Context
+// was not properly initialized, or if there
+// was an error retrieving parameters from the Request
 func (c *Context) GetBool(key string) (bool, error) {
 	v, err := c.Get(key)
 	if err != nil {
@@ -73,8 +80,9 @@ func (c *Context) GetBool(key string) (bool, error) {
 }
 
 // GetFloat64 retrieves the value associated with key as
-// a float64 or returns an error if the conversion failed or
-// Context was not properly initialized.
+// a float64 or returns an error if the conversion failed,
+// Context was not properly initialized, or if there
+// was an error retrieving parameters from the Request
 func (c *Context) GetFloat64(key string) (float64, error) {
 	v, err := c.Get(key)
 	if err != nil {
@@ -84,8 +92,9 @@ func (c *Context) GetFloat64(key string) (float64, error) {
 }
 
 // GetInt64 retrieves the value associated with key as
-// an int64 or returns an error if the conversion failed or
-// Context was not properly initialized.
+// an int64 or returns an error if the conversion failed,
+// Context was not properly initialized, or if there
+// was an error retrieving parameters from the Request
 func (c *Context) GetInt64(key string) (int64, error) {
 	v, err := c.Get(key)
 	if err != nil {
@@ -96,63 +105,69 @@ func (c *Context) GetInt64(key string) (int64, error) {
 
 // Set associates a request parameter value with key.
 // Set returns an error if Context was not properly
-// initialized.
+// initialized or if there
+// was an error retrieving parameters from the Request
 func (c *Context) Set(key, value string) error {
 	if c.Request == nil {
 		return ErrContextNotInitialized
 	}
 
 	if c.params == nil {
-		c.params = c.Request.URL.Query()
+		if err := c.Request.ParseForm(); err != nil {
+			return err
+		}
+		c.params = c.Request.Form
 	}
-
 	c.params.Set(key, value)
-	c.Request.URL.RawQuery = c.params.Encode()
-	c.params = c.Request.URL.Query()
-
+	c.Request.PostForm.Set(key, value)
 	return nil
 }
 
 // SetMulti associates multiple parameter values with key.
 // SetMulti returns an error if Context was not properly
-// initialized.
+// initialized or if there
+// was an error retrieving parameters from the Request
 func (c *Context) SetMulti(key string, values []string) error {
 	if c.Request == nil {
 		return ErrContextNotInitialized
 	}
 
 	if c.params == nil {
-		c.params = c.Request.URL.Query()
+		if err := c.Request.ParseForm(); err != nil {
+			return err
+		}
+		c.params = c.Request.Form
 	}
 
 	for _, v := range values {
 		c.params.Add(key, v)
+		c.Request.PostForm.Add(key, v)
 	}
-	c.Request.URL.RawQuery = c.params.Encode()
-	c.params = c.Request.URL.Query()
-
 	return nil
 }
 
 // SetBool attempts to associate value with key and returns
-// an error if the conversion failed or Context was not properly
-// initialized.
+// an error if the conversion failed, Context was not properly
+// initialized, or if there
+// was an error retrieving parameters from the Request
 func (c *Context) SetBool(key string, value bool) error {
 	v := strconv.FormatBool(value)
 	return c.Set(key, v)
 }
 
 // SetFloat64 attempts to associate value with key and returns
-// an error if the conversion failed or Context was not properly
-// initialized.
+// an error if the conversion failed, Context was not properly
+// initialized, or if there
+// was an error retrieving parameters from the Request
 func (c *Context) SetFloat64(key string, value float64, fmt byte, prec int) error {
 	v := strconv.FormatFloat(value, fmt, prec, 64)
 	return c.Set(key, v)
 }
 
 // SetInt64 attempts to associate value with key and returns
-// an error if the conversion failed or Context was not properly
-// initialized.
+// an error if the conversion failed, Context was not properly
+// initialized, or if there
+// was an error retrieving parameters from the Request
 func (c *Context) SetInt64(key string, value int64) error {
 	v := strconv.FormatInt(value, 10)
 	return c.Set(key, v)
